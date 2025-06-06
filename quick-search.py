65ef5d3235dab3d398fb92494e8bce2d636b4acb
Jordan Stuckman <jstuckman@highwaycommercial.com>
2025-06-06 16:55:38 -0400

    new window funct, added google search

--- quick-search.py
+++ quick-search.py
@@ -1,9 +1,12 @@
 import webbrowser
 import urllib.parse
+import subprocess
+import time
 
 def main():
     name = input("Enter Name: ").strip()
     address = input("Enter Address: ").strip()
+    location = input("Enter City and State: ").strip()
     asset = input("Enter Asset: ").strip()
     business_name = input("Enter Business Name: ").strip()
 
@@ -13,16 +16,19 @@ def main():
         "Zillow": address,
         "Safer": business_name,
         "Truck Paper": asset,
+        "Google": name + " " + location,
     }
 
     SEARCH_SITES = {
-        "OFAC": "https://sanctionssearch.ofac.treas.gov/",
-        "NSOPW": "https://www.nsopw.gov/en/search-results?searchType=all&query={query}",
+        # "OFAC": "https://sanctionssearch.ofac.treas.gov/",
+        # "NSOPW": "https://www.nsopw.gov/en/search-results?searchType=all&query={query}",
         "Zillow": "https://www.zillow.com/homes/{query}_rb/",
-        "Safer": "https://safer.fmcsa.dot.gov/CompanySnapshot.aspx?query_string={query}&searchtype=ANY",
+        "Safer": "https://safer.fmcsa.dot.gov/keywordx.asp?searchstring=%2A{query}%2A&SEARCHTYPE=",
         "Truck Paper": "https://www.truckpaper.com/listings?keywords={query}",
+        "Google": "https://www.google.com/search?q={query}",
     }
 
+    first = True
     for name, url_pattern in SEARCH_SITES.items():
         query = queries[name]
         if not query:
@@ -31,7 +37,12 @@ def main():
         encoded_query = urllib.parse.quote_plus(query)
         url = url_pattern.format(query=encoded_query)
         print(f"Opening {name}...")
-        webbrowser.open_new_tab(url)
+        if first:
+            subprocess.Popen(f'start chrome --new-window "{url}"', shell=True)
+            first = False
+            time.sleep(0.5)  # Wait a moment to ensure the first window opens before opening new tabs
+        else:
+            subprocess.Popen(f'start chrome --new-tab "{url}"', shell=True)
 
 if __name__ == "__main__":
     main()
